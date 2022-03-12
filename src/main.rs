1b6499d134166376219be8463438f153132b18ac
jongregis <jon.gregis@gmail.com>
2022-03-12 09:47:21 -0600

    adding test command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::env;
 use dotenv::dotenv;
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use std::env;
 
 const HELP_MESSAGE: &str = "
 Hello there, Human!
@@ -20,19 +20,23 @@ I hope that resolves your issue!
 ";
 
 const HELP_COMMAND: &str = "!help";
+const TEST_COMMAND: &str = "!test";
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == HELP_COMMAND {
-            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
-                println!("Error sending message: {:?}", why);
+        match &msg.content[..] {
+            HELP_COMMAND => {
+                if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                    println!("Error sending message: {:?}", why);
+                }
             }
+            TEST_COMMAND => println!("{}", msg.content),
+            _ => println!("no command"),
         }
     }
-
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -41,9 +45,7 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     dotenv().expect(".env file not found");
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
-    
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .await
